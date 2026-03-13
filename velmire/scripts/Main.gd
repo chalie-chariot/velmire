56fef203ce337126c8061b15a3e1253b597b0cdd
chalie-chariot <chaliechariot@gmail.com>
2026-03-13 12:33:55 +0900

    ui 보완 01

--- velmire/scripts/Main.gd
+++ velmire/scripts/Main.gd
@@ -1,4 +1,101 @@
 extends Node2D
 
+var _left_tween: Tween
+var _right_tween: Tween
+var _left_open: bool = false
+var _right_open: bool = false
+
 func _ready() -> void:
 	print("Main 시작")
+	$CanvasLayer/LeftPanel.modulate = Color(1, 1, 1, 0)
+	$CanvasLayer/RightPanel.modulate = Color(1, 1, 1, 0)
+	$CanvasLayer/LeftPanel.position = Vector2(-220, $CanvasLayer/LeftPanel.position.y)
+	$CanvasLayer/RightPanel.position = Vector2(1920, $CanvasLayer/RightPanel.position.y)
+	$CanvasLayer/LeftTab.gui_input.connect(_on_left_tab_gui_input)
+	$CanvasLayer/RightTab.gui_input.connect(_on_right_tab_gui_input)
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventKey and event.pressed:
+		if event.keycode == KEY_Q:
+			if _left_open:
+				_slide_left_close()
+			else:
+				_slide_left_open()
+			_left_open = !_left_open
+
+		if event.keycode == KEY_E:
+			if _right_open:
+				_slide_right_close()
+			else:
+				_slide_right_open()
+			_right_open = !_right_open
+
+func _on_left_tab_gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		_on_left_tab_clicked()
+
+func _on_right_tab_gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		_on_right_tab_clicked()
+
+func _on_left_tab_clicked() -> void:
+	if _left_open:
+		_slide_left_close()
+	else:
+		_slide_left_open()
+	_left_open = !_left_open
+
+func _on_right_tab_clicked() -> void:
+	if _right_open:
+		_slide_right_close()
+	else:
+		_slide_right_open()
+	_right_open = !_right_open
+
+func _slide_left_open() -> void:
+	if _left_tween:
+		_left_tween.kill()
+	_left_tween = create_tween()
+	_left_tween.set_parallel(true)
+	_left_tween.tween_property(
+		$CanvasLayer/LeftPanel, "position", Vector2(40, $CanvasLayer/LeftPanel.position.y), 0.25
+	).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_QUART)
+	_left_tween.tween_property(
+		$CanvasLayer/LeftPanel, "modulate", Color(1, 1, 1, 1), 0.25
+	).set_ease(Tween.EASE_OUT)
+
+func _slide_left_close() -> void:
+	if _left_tween:
+		_left_tween.kill()
+	_left_tween = create_tween()
+	_left_tween.set_parallel(true)
+	_left_tween.tween_property(
+		$CanvasLayer/LeftPanel, "position", Vector2(-220, $CanvasLayer/LeftPanel.position.y), 0.25
+	).set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_QUART)
+	_left_tween.tween_property(
+		$CanvasLayer/LeftPanel, "modulate", Color(1, 1, 1, 0), 0.25
+	).set_ease(Tween.EASE_IN)
+
+func _slide_right_open() -> void:
+	if _right_tween:
+		_right_tween.kill()
+	_right_tween = create_tween()
+	_right_tween.set_parallel(true)
+	_right_tween.tween_property(
+		$CanvasLayer/RightPanel, "position", Vector2(1640, $CanvasLayer/RightPanel.position.y), 0.25
+	).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_QUART)
+	_right_tween.tween_property(
+		$CanvasLayer/RightPanel, "modulate", Color(1, 1, 1, 1), 0.25
+	).set_ease(Tween.EASE_OUT)
+
+func _slide_right_close() -> void:
+	if _right_tween:
+		_right_tween.kill()
+	_right_tween = create_tween()
+	_right_tween.set_parallel(true)
+	_right_tween.tween_property(
+		$CanvasLayer/RightPanel, "position", Vector2(1920, $CanvasLayer/RightPanel.position.y), 0.25
+	).set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_QUART)
+	_right_tween.tween_property(
+		$CanvasLayer/RightPanel, "modulate", Color(1, 1, 1, 0), 0.25
+	).set_ease(Tween.EASE_IN)
